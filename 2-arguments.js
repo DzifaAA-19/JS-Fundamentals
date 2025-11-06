44e34972e89ac5501a80c950a61034280fa65766
DzifaAA-19 <agbakpedzifaaku3@gmail.com>
2025-11-06 23:25:08 +0000

    Add 2-arguments.js script

---
+++ 2-arguments.js
@@ -0,0 +1,10 @@
+const args = process.argv;
+const argCount = args.length -2
+
+if (argCount === 0) {
+    console.log("No argument");
+} else if (argCount === 1) {
+    console.log("Argument found");
+} else{
+   console.log("Argument found");
+}
